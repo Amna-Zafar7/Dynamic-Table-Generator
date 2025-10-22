6b363f3f12d09ebbcb8db0297dffa5777c85dd9e
Amna-Zafar7 <zaryabzayan982@gmail.com>
2025-10-22 09:26:29 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+ function Genrate(){
+    let Value = document.
+    getElementById("number");
+
+    let UserValue =Value.value;
+    // initialization condition
+    for (let i =1;  i<=10; i++){
+        console.log(i);
+        document.write(`${UserValue} X ${i} = ${UserValue*i}<br> `)
+    }
+ }
